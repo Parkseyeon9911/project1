a3006dadc65e91d7c4de2b838493a525050c9d70
Parkseyeon9911 <dudal5305@naver.com>
2020-06-16 03:25:51 +0900

    20200616 update

--- m/js/wwa.js
+++ m/js/wwa.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function(){
     
-    $(window).on('scroll',function(){
-        var now_scroll = $(window).scrollTop();
+    $('#wrap').on('scroll',function(){
+        var now_scroll = $('#wrap').scrollTop();
         if(now_scroll > $('.infomation_box li').eq(1).offset().top){
             var black_acc = (now_scroll-$('.infomation_box li').eq(1).offset().top)/50/5
             console.log(black_acc)
 
             $('article').css({
-                backgroundColor:'rgba(0,0,0,'+(1-black_acc)+')'
+                backgroundColor:'rgba(0,0,0,'+(1-(black_acc/2))+')'
             })
             }
         else{
